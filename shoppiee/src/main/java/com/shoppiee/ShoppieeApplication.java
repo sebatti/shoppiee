e31a3000b8a112963ba4093cba60d670ecf52973
sebatti <sebinj353@gmail.com>
2022-04-04 23:17:12 +0530

    Initial commit

---
+++ shoppiee/src/main/java/com/shoppiee/ShoppieeApplication.java
@@ -0,0 +1,13 @@
+package com.shoppiee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppieeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppieeApplication.class, args);
+	}
+
+}
---
+++ shoppiee/src/test/java/com/shoppiee/ShoppieeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppiee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppieeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
